9571b81ad986bd33d2a18eae067f6322c9ebd6b4
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2026-05-20 18:26:06 -0400

    Creation of category context

--- FullTractor.Api/Program.cs
+++ FullTractor.Api/Program.cs
@@ -1,8 +1,13 @@
+using FullTractor.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DbCon") ?? throw new InvalidOperationException("Connection string 'DbCon' not found.");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+// Add context to the project.
+builder.Services.AddDbContext<FullTractorContext>(options => options.UseSqlServer(connectionString));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
